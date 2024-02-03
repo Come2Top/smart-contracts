e54f4f3201f6d5800e26daf7ad831c66255c0821
FarajiOranj <farajioranj@gmail.com>
2024-02-03 07:22:10 +0330

    some vars names fixed, + contract name changed + functions names and paramas fixed

--- src/256.sol
+++ src/256.sol
@@ -36,7 +36,7 @@ pragma solidity 0.8.18;
 
 import {IUSDC} from "./interfaces/IUSDC.sol";
 
-contract $256$ {
+contract TwoHundredFiftySix {
     /*******************************\
     |-*-*-*-*-*   TYPES   *-*-*-*-*-|
     \*******************************/
@@ -51,7 +51,7 @@ contract $256$ {
         uint8 soldTickets;
         uint8 updatedWave;
         uint216 startedBN;
-        bytes BYTES256;
+        bytes tickets;
     }
 
     /********************************\
@@ -63,21 +63,21 @@ contract $256$ {
     uint80 private _neededUSDC;
     uint160 private _currentGameID;
 
-    mapping(uint256 => GameData) private gameConfig;
-    mapping(uint256 => mapping(uint256 => address)) private ticketOwnership;
-    mapping(uint256 => mapping(address => uint256)) private totalPlayerTickets;
+    mapping(uint256 => GameData) private _gameConfig;
+    mapping(uint256 => mapping(uint256 => address)) private _ticketOwnership;
+    mapping(uint256 => mapping(address => uint256)) private _totalPlayerTickets;
 
     /*******************************\
     |-*-*-*-*   CONSTANTS   *-*-*-*-|
     \*******************************/
 
-    IUSDC private immutable USDC;
-    address private immutable ADMIN;
-    uint256 private constant FEE = 23438;
-    uint256 private constant BASIS = 1000000;
-    uint256 private constant WAVE_DURATION = 80;
-    uint256 private constant MAX_PARTIES = 256;
-    bytes private constant BYTES256 =
+    IUSDC private immutable _usdc;
+    address private immutable _admin;
+    uint256 private constant _FEE = 23438;
+    uint256 private constant _BASIS = 1000000;
+    uint256 private constant _WAVE_DURATION = 80;
+    uint256 private constant _MAX_PARTIES = 256;
+    bytes private constant _TICKET256 =
         hex"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
 
     /********************************\
@@ -114,13 +114,13 @@ contract $256$ {
     \*******************************/
 
     modifier onlyAdmin() {
-        require(msg.sender == ADMIN, "ONLY_ADMIN_FUNCTION");
+        require(msg.sender == _admin, "ONLY_ADMIN_FUNCTION");
         _;
     }
 
     modifier onlyPaused() {
         require(
-            gameConfig[_currentGameID].eligibleWaveWithdrawns == -1 &&
+            _gameConfig[_currentGameID].eligibleWaveWithdrawns == -1 &&
                 _pausy == true,
             "ONLY_PAUSED"
         );
@@ -146,12 +146,12 @@ contract $256$ {
         require(mtpg_ != 0 && neededUSDC_ != 0, "ZERO_UINT_PROVIDED");
         _onlyPow2(mtpg_);
 
-        USDC = IUSDC(usdc_);
-        ADMIN = admin_;
+        _usdc = IUSDC(usdc_);
+        _admin = admin_;
         _maxTicketsPerGame = mtpg_;
         _neededUSDC = neededUSDC_;
 
-        gameConfig[0].BYTES256 = BYTES256;
+        _gameConfig[0].tickets = _TICKET256;
     }
 
     /*******************************\
@@ -188,67 +188,67 @@ contract $256$ {
     \******************************/
 
     function joinGame(uint8[] calldata ticketIDs) external {
-        GameData storage GD;
+        GameData storage gameData;
         address sender = msg.sender;
         uint256 gameId = _currentGameID;
         uint256 ticketLimit = _maxTicketsPerGame + 1;
         uint256 neededUSDC = _neededUSDC;
         uint256 totalTickets = ticketIDs.length;
 
-        if (gameConfig[gameId].eligibleWaveWithdrawns == -1) {
+        if (_gameConfig[gameId].eligibleWaveWithdrawns == -1) {
             unchecked {
                 gameId++;
                 _currentGameID++;
             }
-            GD = gameConfig[gameId];
-            GD.BYTES256 = BYTES256;
-        } else GD = gameConfig[gameId];
+            gameData = _gameConfig[gameId];
+            gameData.tickets = _TICKET256;
+        } else gameData = _gameConfig[gameId];
 
-        uint256 remainingTickets = MAX_PARTIES - GD.soldTickets;
-        bytes memory GDB = GD.BYTES256;
+        uint256 remainingTickets = _MAX_PARTIES - gameData.soldTickets;
+        bytes memory tickets = gameData.tickets;
 
-        require(_pausy == false || GD.soldTickets != 0, "ONLY_UNPAUSED");
+        require(_pausy == false || gameData.soldTickets != 0, "ONLY_UNPAUSED");
         require(
             totalTickets != 0 && totalTickets < ticketLimit,
             "CHECK_TICKETS_ARRAY"
         );
-        require(GD.startedBN == 0, "WAIT_FOR_NEXT_MATCH");
+        require(gameData.startedBN == 0, "WAIT_FOR_NEXT_MATCH");
         require(
-            totalTickets + totalPlayerTickets[gameId][sender] < ticketLimit,
+            totalTickets + _totalPlayerTickets[gameId][sender] < ticketLimit,
             "PARTICIPATED_BEFORE"
         );
         require(totalTickets < remainingTickets, "OUT_OF_TICKETS");
         require(
-            !(USDC.allowance(sender, address(this)) <
+            !(_usdc.allowance(sender, address(this)) <
                 (totalTickets * neededUSDC)),
             "APPROVE_NEEDED"
         );
 
         for (uint256 i; i < totalTickets; ) {
             if (ticketIDs[i] == 0) {
-                require(GDB[0] != 0xff, "TICKET_RESERVED");
-                GDB[0] = 0xff;
+                require(tickets[0] != 0xff, "TICKET_RESERVED");
+                tickets[0] = 0xff;
             } else {
-                require(GDB[ticketIDs[i]] != 0x00, "TICKET_RESERVED");
-                GDB[ticketIDs[i]] = 0x00;
+                require(tickets[ticketIDs[i]] != 0x00, "TICKET_RESERVED");
+                tickets[ticketIDs[i]] = 0x00;
             }
 
             unchecked {
-                ticketOwnership[gameId][ticketIDs[i]] = sender;
+                _ticketOwnership[gameId][ticketIDs[i]] = sender;
                 i++;
             }
         }
-        totalPlayerTickets[gameId][sender] += totalTickets;
+        _totalPlayerTickets[gameId][sender] += totalTickets;
 
-        USDC.transferFrom(sender, address(this), (totalTickets * neededUSDC));
+        _usdc.transferFrom(sender, address(this), (totalTickets * neededUSDC));
 
-        GD.BYTES256 = GDB;
+        gameData.tickets = tickets;
 
         if (totalTickets == remainingTickets) {
             uint256 blockNo = block.number;
-            GD.startedBN = uint216(blockNo);
-            GD.BYTES256 = BYTES256;
-            emit GameStarted(gameId, blockNo, MAX_PARTIES * neededUSDC);
+            gameData.startedBN = uint216(blockNo);
+            gameData.tickets = _TICKET256;
+            emit GameStarted(gameId, blockNo, _MAX_PARTIES * neededUSDC);
         }
     }
 
@@ -256,7 +256,7 @@ contract $256$ {
         uint256 fee;
         address sender = msg.sender;
         uint256 gameId = _currentGameID;
-        uint256 balance = USDC.balanceOf(address(this));
+        uint256 balance = _usdc.balanceOf(address(this));
         uint256 length = indexes.length;
 
         (
@@ -277,19 +277,19 @@ contract $256$ {
         );
 
         if (tickets.length < 3) {
-            fee = (balance * FEE) / BASIS;
+            fee = (balance * _FEE) / _BASIS;
 
-            gameConfig[gameId].BYTES256 = tickets;
-            gameConfig[gameId].eligibleWaveWithdrawns = -1;
+            _gameConfig[gameId].tickets = tickets;
+            _gameConfig[gameId].eligibleWaveWithdrawns = -1;
 
-            USDC.transfer(ADMIN, fee);
+            _usdc.transfer(_admin, fee);
 
             if (tickets.length == 1) {
-                address ticketOwner = ticketOwnership[gameId][
+                address ticketOwner = _ticketOwnership[gameId][
                     uint8(tickets[0])
                 ];
 
-                USDC.transfer(ticketOwner, balance - fee);
+                _usdc.transfer(ticketOwner, balance - fee);
 
                 emit GameFinished(
                     gameId,
@@ -299,18 +299,18 @@ contract $256$ {
                 );
             } else {
                 require(
-                    ticketOwnership[gameId][uint8(tickets[indexes[0]])] ==
+                    _ticketOwnership[gameId][uint8(tickets[indexes[0]])] ==
                         sender,
                     "OWNERSHIP_REQUESTED"
                 );
 
-                address winner1 = ticketOwnership[gameId][uint8(tickets[0])];
-                address winner2 = ticketOwnership[gameId][uint8(tickets[1])];
+                address winner1 = _ticketOwnership[gameId][uint8(tickets[0])];
+                address winner2 = _ticketOwnership[gameId][uint8(tickets[1])];
                 uint256 winner2Amount = (balance - fee) / 2;
                 uint256 winner1Amount = balance - fee - winner2Amount;
 
-                USDC.transfer(winner1, winner1Amount);
-                USDC.transfer(winner2, winner2Amount);
+                _usdc.transfer(winner1, winner1Amount);
+                _usdc.transfer(winner2, winner2Amount);
 
                 emit GameFinished(
                     gameId,
@@ -324,7 +324,7 @@ contract $256$ {
             uint256[] memory ticketIds = new uint256[](length);
 
             require(
-                ticketOwnership[gameId][uint8(tickets[indexes[0]])] == sender,
+                _ticketOwnership[gameId][uint8(tickets[indexes[0]])] == sender,
                 "OWNERSHIP_REQUESTED"
             );
             require(indexes[0] < tickets.length, "INDEX_OUT_OF_BOUNDS");
@@ -333,7 +333,7 @@ contract $256$ {
 
                 updatedTickets = _deleteOneIndex(indexes[0], tickets);
             } else {
-                updatedTickets = this._returnBytedCalldataArray(
+                updatedTickets = this.returnBytedCalldataArray(
                     tickets,
                     0,
                     indexes[0]
@@ -346,7 +346,7 @@ contract $256$ {
                     );
                     require(indexes[i] < tickets.length, "INDEX_OUT_OF_BOUNDS");
                     require(
-                        ticketOwnership[gameId][uint8(tickets[indexes[i]])] ==
+                        _ticketOwnership[gameId][uint8(tickets[indexes[i]])] ==
                             sender,
                         "OWNERSHIP_REQUESTED"
                     );
@@ -355,7 +355,7 @@ contract $256$ {
 
                     updatedTickets = abi.encodePacked(
                         updatedTickets,
-                        this._returnBytedCalldataArray(
+                        this.returnBytedCalldataArray(
                             tickets,
                             indexes[i - 1] + 1,
                             indexes[i]
@@ -371,7 +371,7 @@ contract $256$ {
                 if (indexes[indexes.length - 1] < length - 1) {
                     updatedTickets = abi.encodePacked(
                         updatedTickets,
-                        this._returnBytedCalldataArray(
+                        this.returnBytedCalldataArray(
                             tickets,
                             indexes[length - 1] + 1,
                             tickets.length
@@ -381,22 +381,22 @@ contract $256$ {
             }
 
             uint256 idealWinnerPrize = (balance / tickets.length) * length;
-            fee = (idealWinnerPrize * FEE) / BASIS;
+            fee = (idealWinnerPrize * _FEE) / _BASIS;
 
-            USDC.transfer(sender, idealWinnerPrize - fee);
-            USDC.transfer(ADMIN, fee);
+            _usdc.transfer(sender, idealWinnerPrize - fee);
+            _usdc.transfer(_admin, fee);
 
             unchecked {
                 eligibleWaveWithdrawns -= int256(length);
             }
 
-            gameConfig[gameId].BYTES256 = updatedTickets;
-            gameConfig[gameId].eligibleWaveWithdrawns = int8(
+            _gameConfig[gameId].tickets = updatedTickets;
+            _gameConfig[gameId].eligibleWaveWithdrawns = int8(
                 eligibleWaveWithdrawns
             );
 
-            if (gameConfig[gameId].updatedWave != currentWave)
-                gameConfig[gameId].updatedWave = uint8(currentWave);
+            if (_gameConfig[gameId].updatedWave != currentWave)
+                _gameConfig[gameId].updatedWave = uint8(currentWave);
 
             emit GameUpdated(gameId, sender, idealWinnerPrize - fee, ticketIds);
         }
@@ -417,23 +417,23 @@ contract $256$ {
         )
     {
         uint256 gameId = _currentGameID;
-        GameData memory GD = gameConfig[gameId];
+        GameData memory gameData = _gameConfig[gameId];
 
-        eligibleWaveWithdrawns = GD.eligibleWaveWithdrawns;
-        currentWave = GD.updatedWave;
-        tickets = GD.BYTES256;
+        eligibleWaveWithdrawns = gameData.eligibleWaveWithdrawns;
+        currentWave = gameData.updatedWave;
+        tickets = gameData.tickets;
 
-        if (GD.startedBN == 0 || GD.eligibleWaveWithdrawns == -1)
-            stat = GD.startedBN == 0 ? Status.notStarted : Status.finished;
+        if (gameData.startedBN == 0 || gameData.eligibleWaveWithdrawns == -1)
+            stat = gameData.startedBN == 0 ? Status.notStarted : Status.finished;
         else {
             stat = Status.inProcess;
 
             uint256 randomSeed;
             uint256 bn = block.number;
-            uint256 lastWave = GD.updatedWave == 0 ? 1 : GD.updatedWave + 1;
+            uint256 lastWave = gameData.updatedWave == 0 ? 1 : gameData.updatedWave + 1;
 
-            while ((lastWave * WAVE_DURATION) + GD.startedBN < bn) {
-                randomSeed = _getRandomSeed(bn + (lastWave * WAVE_DURATION));
+            while ((lastWave * _WAVE_DURATION) + gameData.startedBN < bn) {
+                randomSeed = _getRandomSeed(bn + (lastWave * _WAVE_DURATION));
                 tickets = _bytedArrayShuffler(
                     tickets,
                     randomSeed,
@@ -451,6 +451,14 @@ contract $256$ {
                 }
             }
         }
+    }
+
+    function returnBytedCalldataArray(
+        bytes calldata array,
+        uint256 from,
+        uint256 to
+    ) external pure returns (bytes memory) {
+        return array[from:to];
     }
 
     /*****************************\
@@ -473,7 +481,7 @@ contract $256$ {
             }
         }
 
-        return this._returnBytedCalldataArray(_array, 0, _to);
+        return this.returnBytedCalldataArray(_array, 0, _to);
     }
 
     function _deleteOneIndex(uint8 _index, bytes memory _bytesArray)
@@ -484,20 +492,20 @@ contract $256$ {
         return
             _index != (_bytesArray.length - 1)
                 ? abi.encodePacked(
-                    this._returnBytedCalldataArray(_bytesArray, 0, _index),
-                    this._returnBytedCalldataArray(
+                    this.returnBytedCalldataArray(_bytesArray, 0, _index),
+                    this.returnBytedCalldataArray(
                         _bytesArray,
                         _index + 1,
                         _bytesArray.length
                     )
                 )
-                : this._returnBytedCalldataArray(_bytesArray, 0, _index);
+                : this.returnBytedCalldataArray(_bytesArray, 0, _index);
     }
 
-    function _getRandomSeed(uint256 startBlock) public view returns (uint256) {
+    function _getRandomSeed(uint256 startBlock) private view returns (uint256) {
         require(!(startBlock > block.number), "WAITING_FOR_NEXT_WAVE");
 
-        uint256 b = WAVE_DURATION;
+        uint256 b = _WAVE_DURATION;
         uint256 index = 20;
 
         uint256[] memory parts = new uint256[](5);
@@ -565,13 +573,5 @@ contract $256$ {
 
     function _revertOnZeroUint(uint256 integer) private pure {
         require(integer != 0, "ZERO_UINT_PROVIDED");
-    }
-
-    function _returnBytedCalldataArray(
-        bytes calldata _array,
-        uint256 _from,
-        uint256 _to
-    ) external pure returns (bytes memory) {
-        return _array[_from:_to];
     }
 }
