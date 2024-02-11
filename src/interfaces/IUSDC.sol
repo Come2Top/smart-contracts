bf43f565c3c9f84d6538e8436738f6229b6b8e28
FarajiOranj <farajioranj@gmail.com>
2024-02-12 03:09:05 +0330

    IUSDC inherits from IERC20 now

--- src/interfaces/IUSDC.sol
+++ src/interfaces/IUSDC.sol
@@ -1,65 +1,10 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: --256--
 pragma solidity 0.8.18;
 
+import {IERC20} from "./IERC20.sol";
+
 /// @notice Interface of the ERC20 standard as defined in the EIP.
-interface IUSDC {
-    /// @dev Returns the amount of tokens in existence.
-    function totalSupply() external view returns (uint256);
-
-    /// @dev Returns the amount of tokens owned by `account`.
-    function balanceOf(address account) external view returns (uint256);
-
-    /**
-        @dev Moves `amount` tokens from the caller's account to `to`.
-    
-        Emits a {Transfer} event.
-
-        Returns a boolean value indicating whether the operation succeeded.
-    */
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    /**
-        @dev Returns the remaining number of tokens that `spender` will be
-        allowed to spend on behalf of `owner` through {transferFrom}. This is
-        zero by default.
-
-        This value changes when {approve} or {transferFrom} are called.
-    */
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    /**
-        @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-
-        Emits an {Approval} event.
-
-        Returns a boolean value indicating whether the operation succeeded.
-
-        IMPORTANT: Beware that changing an allowance with this method brings the risk
-        that someone may use both the old and the new allowance by unfortunate
-        transaction ordering. One possible solution to mitigate this race
-        condition is to first reduce the spender's allowance to 0 and set the
-        desired value afterwards:
-        https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-        @dev Moves `amount` tokens from `from` to `to` using the
-        allowance mechanism. `amount` is then deducted from the caller's allowance.
-
-        Emits a {Transfer} event.
-
-        Returns a boolean value indicating whether the operation succeeded.
-    */
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-
+interface IUSDC is IERC20 {
     /**
         @dev Checks if account is blacklisted
 
