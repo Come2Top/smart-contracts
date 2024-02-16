d522877c6709980543076cb42e9818b1dcda3c2c
FarajiOranj <farajioranj@gmail.com>
2024-02-16 22:36:35 +0330

    files updated

---
+++ script/Deploy.s.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract Deploy is Script {
+    function setUp() external {}
+
+    function run() external {}
+}
