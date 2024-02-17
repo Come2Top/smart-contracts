497509804a63559cce06cb4aa36db62bb001e289
FarajiOranj <farajioranj@gmail.com>
2024-02-17 16:11:32 +0330

    console added to script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.18;
 import {Script, console} from "forge-std/Script.sol";
 
 contract Deploy is Script {
-    function setUp() external {}
-
+    using console for *;
     function run() external {}
 }
