16646feeb039dd4e4a593fee1739236179a47dad
FarajiOranj <farajioranj@gmail.com>
2024-02-29 01:58:08 +0330

    C2Treasury Coveredup by natspec comments

--- contracts/C2Treasury.sol
+++ contracts/C2Treasury.sol
@@ -1,6 +1,14 @@
 //  SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
+/**
+    @author @FarajiOranj
+    @custom:auditor @MatinR1
+
+    @title Come2Top Offerors Treasury.
+    @dev Contract will be used by Come2Top contract
+        as a seperate Treasury for Offerors.
+*/
 contract C2Treasury {
     bool private _$;
 
