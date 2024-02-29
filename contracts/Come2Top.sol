be020a65f82e5287b3f8b1f69e5742c0a83d0e13
FarajiOranj <farajioranj@gmail.com>
2024-02-29 07:45:40 +0330

    ticket256 changed as function

--- contracts/Come2Top.sol
+++ contracts/Come2Top.sol
@@ -76,8 +76,6 @@ contract Come2Top {
     uint256 public constant OFFEREE_BENEFICIARY = 95;
     uint256 public constant WAVE_DURATION = 93;
     uint256 public constant MAX_TICKETS_PER_GAME = 8;
-    bytes public constant TICKET256 =
-        hex"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
 
     /********************************\
     |-*-*-*-*-*   EVENTS   *-*-*-*-*-|
@@ -211,7 +209,7 @@ contract Come2Top {
         USDT = IUSDT(usdt);
         TREASURY = treasury;
 
-        gameData[0].tickets = TICKET256;
+        gameData[0].tickets = TICKET256();
 
         (bool ok, ) = treasury.call(abi.encode(usdt));
         if (!ok) revert APROVE_OPERATION_FAILED();
@@ -287,7 +285,7 @@ contract Come2Top {
                 currentGameID++;
             }
             GD = gameData[gameID];
-            GD.tickets = TICKET256;
+            GD.tickets = TICKET256();
         } else GD = gameData[gameID];
 
         uint256 remainingTickets = MAX_PARTIES - GD.soldTickets;
@@ -347,7 +345,7 @@ contract Come2Top {
         if (totalTickets == remainingTickets) {
             uint256 currentBlock = block.number;
             GD.startedBlock = uint216(currentBlock);
-            GD.tickets = TICKET256;
+            GD.tickets = TICKET256();
             emit GameStarted(gameID, currentBlock, MAX_PARTIES * neededUSDT);
         } else GD.soldTickets += uint8(totalTickets);
     }
@@ -1031,6 +1029,15 @@ contract Come2Top {
         if (!found) revert ONLY_WINNER_TICKET(ticketID);
         return index;
     }
+
+    /**
+        @dev Generates a byte array with 256 elements, each element representing a unique ticket ID.
+        @return bytes The byte array containing the 256 ticket IDs.
+    */
+    function TICKET256() private pure returns (bytes memory) {
+        return
+            hex"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+    }
 }
 
 /// @notice IUSD interface, which is used for easier interactions with USDT contract.
@@ -1041,9 +1048,7 @@ interface IUSDT {
         @param to The address to which the USDT tokens will be transferred.
         @param amount The amount of USDT tokens to be transferred.
     */
-    function transfer(address to, uint256 amount)
-        external
-        returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
 
     /**
         @notice Allows the contract to transfer USDT tokens from one address to another.
