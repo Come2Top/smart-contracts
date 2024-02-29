5c93645dbf009aa54fc33a0a38386f3df26e40e1
FarajiOranj <farajioranj@gmail.com>
2024-02-29 07:48:01 +0330

    script removed

--- script/Deploy.s.sol
+++
@@ -1,9 +0,0 @@
-//  SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
-
-import {Script, console} from "forge-std/Script.sol";
-
-contract Deploy is Script {
-    using console for *;
-    function run() external {}
-}
