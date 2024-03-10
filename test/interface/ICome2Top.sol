1630c02833e2e5ab647381c60ed110f204f137a5
FarajiOranj <farajioranj@gmail.com>
2024-03-10 16:27:48 +0330

    formatted

--- test/interface/ICome2Top.sol
+++ test/interface/ICome2Top.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.18;
 
 interface Interface {
+    /*******************************\
+    |-*-*-*-*-*   TYPES   *-*-*-*-*-|
+    \*******************************/
     type Status is uint8;
 
     struct TicketInfo {
@@ -9,84 +12,103 @@ interface Interface {
         address owner;
     }
 
-    error APROVE_OPERATION_FAILED();
-    error CHECK_TICKETS_LENGTH(uint256 ticketLength);
-    error FEWER_TICKETS_LEFT(uint256 remainingTickets);
-    error NO_AMOUNT_TO_REFUND();
-    error OFFER_NOT_FOUND();
-    error ONLY_ADMIN();
-    error ONLY_EOA();
-    error ONLY_HIGHER_THAN_CURRENT_OFFER_VALUE(
-        uint256 offer,
-        uint256 lastOfferValue
-    );
-    error ONLY_HIGHER_THAN_CURRENT_TICKET_VALUE(
-        uint256 offer,
-        uint256 ticketValue
-    );
-    error ONLY_PAUSED_AND_FINISHED_MODE(bool isPaused);
-    error ONLY_TICKET_OWNER(uint256 ticketID);
-    error ONLY_UNPAUSED_OR_TICKET_SALE_MODE(bool isPaused);
-    error ONLY_WINNER_TICKET(uint256 ticketID);
-    error ONLY_WITHDRAWABLE_MODE(Status currentStat);
-    error PARTICIPATED_BEFORE();
-    error PLAYER_HAS_NO_TICKETS();
-    error SLECTED_TICKETS_SOLDOUT_BEFORE();
-    error VALUE_CANT_BE_GREATER_THAN(uint256 givenValue);
-    error VALUE_CANT_BE_LOWER_THAN(uint256 givenValue);
-    error WAIT_FOR_FIRST_WAVE();
-    error WAIT_FOR_NEXT_WAGER_MATCH();
-    error ZERO_ADDRESS_PROVIDED();
-    error ZERO_UINT_PROVIDED();
-
+    /********************************\
+    |-*-*-*-*-*   EVENTS   *-*-*-*-*-|
+    \********************************/
     event TicketsSold(address indexed buyer, bytes ticketIDs);
+
     event WagerStarted(
         uint256 indexed wagerID,
         uint256 indexed startedBlockNo,
         uint256 indexed prizeAmount
     );
+
     event WagerUpdated(
         uint256 indexed wagerID,
         address indexed winner,
         uint256 indexed amount,
         uint256 ticketID
     );
+
     event WagerFinished(
         uint256 indexed wagerID,
         address indexed winner,
         uint256 indexed amount,
         uint256 ticketID
     );
+
     event WagerFinished(
         uint256 indexed wagerID,
         address[2] winners,
         uint256[2] amounts,
         uint256[2] ticketIDs
     );
-    event OfferAccepted(
-        address indexed newOwner,
-        uint256 indexed ticketID,
-        uint256 indexed amount,
-        address lastOwner
-    );
+
     event OfferMade(
         address indexed maker,
         uint256 indexed ticketID,
         uint256 indexed amount,
         address lastOfferor
     );
+
+    event OfferAccepted(
+        address indexed newOwner,
+        uint256 indexed ticketID,
+        uint256 indexed amount,
+        address lastOwner
+    );
+
     event StaleOffersTookBack(
         address indexed maker,
         address indexed to,
         uint256 indexed amount
     );
 
+    /********************************\
+    |-*-*-*-*-*   ERRORS   *-*-*-*-*-|
+    \********************************/
+    error ONLY_EOA();
+    error ONLY_ADMIN();
+    error ONLY_TICKET_OWNER(uint256 ticketID);
+    error ONLY_WINNER_TICKET(uint256 ticketID);
+    error ONLY_WITHDRAWABLE_MODE(Status currentStat);
+    error ONLY_PAUSED_AND_FINISHED_MODE(bool isPaused);
+    error ONLY_UNPAUSED_OR_TICKET_SALE_MODE(bool isPaused);
+    error ONLY_HIGHER_THAN_CURRENT_TICKET_VALUE(
+        uint256 offer,
+        uint256 ticketValue
+    );
+    error ONLY_HIGHER_THAN_CURRENT_OFFER_VALUE(
+        uint256 offer,
+        uint256 lastOfferValue
+    );
+    error APROVE_OPERATION_FAILED();
+    error VALUE_CANT_BE_LOWER_THAN(uint256 givenValue);
+    error VALUE_CANT_BE_GREATER_THAN(uint256 givenValue);
+    error ZERO_ADDRESS_PROVIDED();
+    error ZERO_UINT_PROVIDED();
+    error CHECK_TICKETS_LENGTH(uint256 ticketLength);
+    error FEWER_TICKETS_LEFT(uint256 remainingTickets);
+    error SLECTED_TICKETS_SOLDOUT_BEFORE();
+    error PARTICIPATED_BEFORE();
+    error PLAYER_HAS_NO_TICKETS();
+    error NO_AMOUNT_TO_REFUND();
+    error OFFER_NOT_FOUND();
+    error WAIT_FOR_NEXT_WAGER_MATCH();
+    error WAIT_FOR_FIRST_WAVE();
+
+    /*******************************\
+    |-*-*-*   ADMINSTRATION   *-*-*-|
+    \*******************************/
     function togglePause() external;
 
     function changeTicketPrice(uint80 ticketPrice_) external;
 
     function changeMaxTicketsPerWager(uint8 maxTicketsPerWager_) external;
 
+    /*********************************\
+    |-*-*-*-*   WAGER-LOGIC   *-*-*-*-|
+    \*********************************/
     function joinWager(uint8[] memory ticketIDs) external;
 
     function receiveWagerPrize(uint8 ticketID) external;
@@ -97,17 +119,20 @@ interface Interface {
 
     function takeBackStaleOffers(address to) external;
 
-    function ADMIN() external view returns (address);
-
-    function MAGIC_VALUE() external view returns (uint256);
+    /******************************\
+    |-*-*-*-*-*   VIEW   *-*-*-*-*-|
+    \******************************/
+    function TICKET256() external view returns (bytes memory);
 
     function THIS() external view returns (address);
 
-    function TICKET256() external view returns (bytes memory);
+    function ADMIN() external view returns (address);
+
+    function USDT() external view returns (address);
 
     function TREASURY() external view returns (address);
 
-    function USDT() external view returns (address);
+    function MAGIC_VALUE() external view returns (uint256);
 
     function pause() external view returns (bool);
 
