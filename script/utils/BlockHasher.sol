48cb8f7ed66b38463123263ffab8ad14280f6328
FarajiOranj <farajioranj@gmail.com>
2024-04-13 00:12:59 +0330

    version changed to 0.8.20

--- script/utils/BlockHasher.sol
+++ script/utils/BlockHasher.sol
@@ -1,5 +1,5 @@
-//  SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
+//  SPDX-License-Identifier: -- Come2Top --
+pragma solidity 0.8.20;
 
 contract BlockHasher {
     mapping(uint256 => bytes32) public blockHash;
