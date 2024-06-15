4e331321291b0597b52faceb115434104c165b9e
FarajiOranj <farajioranj@gmail.com>
2024-06-15 17:55:41 +0330

    claim function completed

--- contracts/Come2Top.sol
+++ contracts/Come2Top.sol
@@ -9,8 +9,8 @@ import {CurveMooLib} from "./libraries/CurveMooLib.sol";
 /**
     @author @4bit-lab
     @title Come2Top Main Contract.
-    @notice Come2Top is a secure, automated, and fully decentralized wagering platform
-        built on the Polygon Mainnet, that works without the involvement of third parties.
+    @notice Come2Top is a secure, automated, and fully decentralized GameFi platform
+        built on the Fraxta; Mainnet, that works without the involvement of third parties.
         For more information & further questions, visit: https://come2.top
 */
 contract Come2Top {
@@ -83,13 +83,11 @@ contract Come2Top {
     /*******************************\
     |-*-*-*-*   CONSTANTS   *-*-*-*-|
     \*******************************/
+    uint256 public immutable MAGIC_VALUE;
     IERC20 public immutable TOKEN;
     address public immutable TREASURY;
     address public immutable THIS = address(this);
-    uint256 public immutable MAGIC_VALUE;
-    // Only For TEST!
-    ISuperchainL1Block private constant SuperchainL1Block =
-        ISuperchainL1Block(0xB0B58f5e88957084Ea40CDf17D6E202016e47AfD);
+    ISuperchainL1Block public immutable SUPERCHAIN_L1_BLOCK;
     bytes private constant BYTE_TICKETS =
         hex"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
     uint256 private constant MIN_TICKET_PRICE = 1e19;
@@ -97,7 +95,6 @@ contract Come2Top {
     uint256 private constant WAVE_ELIGIBLES_TIME = 144;
     uint256 private constant SAFTY_DURATION = 48;
     uint256 private constant MIN_PRNG_PERIOD = 12;
-    // uint256 private constant REWARD_BASIS = 1e6;
     uint256 private constant BASIS = 100;
     uint256 private constant OFFEREE_BENEFICIARY = 94;
     uint256 private constant MIN_TICKET_VALUE_OFFER = 10;
@@ -226,14 +223,18 @@ contract Come2Top {
         uint80 tp,
         uint256 prngp,
         address token,
-        address treasury
+        address treasury,
+        address superchainL1Block
     ) {
         _checkMTPW(mtpw);
         _checkTP(tp);
         _checkPRNGP(prngp);
 
-        if (token == ZERO_ADDRESS || treasury == ZERO_ADDRESS)
-            revert ZERO_ADDRESS_PROVIDED();
+        if (
+            token == ZERO_ADDRESS ||
+            treasury == ZERO_ADDRESS ||
+            superchainL1Block == ZERO_ADDRESS
+        ) revert ZERO_ADDRESS_PROVIDED();
 
         owner = msg.sender;
         maxTicketsPerWager = mtpw;
@@ -241,6 +242,7 @@ contract Come2Top {
         prngPeriod = prngp;
         TOKEN = IERC20(token);
         TREASURY = treasury;
+        SUPERCHAIN_L1_BLOCK = ISuperchainL1Block(superchainL1Block);
         gameData[ZERO].tickets = BYTE_TICKETS;
         unchecked {
             MAGIC_VALUE = uint160(address(this)) * block.chainid;
@@ -399,7 +401,7 @@ contract Come2Top {
         emit TicketsSold(sender, realTickets);
 
         if (totalTickets == remainingTickets) {
-            uint64 currentL1Block = SuperchainL1Block.number();
+            uint64 currentL1Block = SUPERCHAIN_L1_BLOCK.number();
             GD.startedL1Block = currentL1Block;
             GD.tickets = BYTE_TICKETS;
             GD.baseBalance = MAX_PARTIES * neededFRAX;
@@ -1129,7 +1131,7 @@ contract Come2Top {
                             uint256(
                                 sha256(
                                     abi.encodePacked(
-                                        SuperchainL1Block.numberToRandao(
+                                        SUPERCHAIN_L1_BLOCK.numberToRandao(
                                             uint64(
                                                 startBlock -
                                                     prngDuration /
@@ -1139,14 +1141,14 @@ contract Come2Top {
                                     )
                                 )
                             ) +
-                                SuperchainL1Block.numberToRandao(
+                                SUPERCHAIN_L1_BLOCK.numberToRandao(
                                     uint64(startBlock - prngDuration / TWO)
                                 ) +
                                 uint160(
                                     ripemd160(
                                         abi.encodePacked(
                                             uint160(
-                                                SuperchainL1Block
+                                                SUPERCHAIN_L1_BLOCK
                                                     .numberToRandao(
                                                         uint64(
                                                             startBlock -
@@ -1214,7 +1216,7 @@ contract Come2Top {
         remainingTickets = GD.tickets;
         eligibleToSell = GD.eligibleToSell;
 
-        uint256 currentL1Block = SuperchainL1Block.number();
+        uint256 currentL1Block = SUPERCHAIN_L1_BLOCK.number();
         bool isClaimable = GD.startedL1Block + L1_BLOCK_WAIT_TIME >
             currentL1Block;
 
