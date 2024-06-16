7b78d72445f5053de20e062558ae38c26a292f6a
FarajiOranj <farajioranj@gmail.com>
2024-06-16 18:18:53 +0330

    dummy beefy coded 40%

---
+++ contracts/mock/DummyBeefyVaultV7.sol
@@ -0,0 +1,32 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {ISuperchainL1Block} from "../interfaces/ISuperchainL1Block.sol";
+
+import {DummyCurveStableSwapNG} from "./DummyCurveStableSwapNG.sol";
+
+contract DummyYieldFarming {
+    string public constant name = "Moo Curve crvUSD-FRAX";
+    string public constant symbol = "mooCurveCrvUSD-FRAX";
+    uint8 public constant decimals = 18;
+    DummyCurveStableSwapNG private immutable CurveStableSwapNG;
+
+    address public come2Top;
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        CurveStableSwapNG = new DummyCurveStableSwapNG();
+    }
+
+    function setCome2Top(address _come2Top) external {
+        require(come2Top == address(0) && _come2Top != address(0));
+
+        come2Top = _come2Top;
+    }
+
+    function deposit(uint amount) external {}
+
+    function withdraw(uint256 shares) external {}
+
+    function getPricePerFullShare() external view returns (uint256) {}
+}
