b095bf54d47feb7dd61d6170733b83ef5165849d
FarajiOranj <farajioranj@gmail.com>
2024-06-23 14:00:21 +0330

    FraxtalL1Block import path fixed

--- contracts/Come2Top.sol
+++ contracts/Come2Top.sol
@@ -2,14 +2,14 @@
 pragma solidity 0.8.20;
 
 import {IERC20} from "./interfaces/IERC20.sol";
-import {ISuperchainL1Block} from "./interfaces/ISuperchainL1Block.sol";
+import {IFraxtalL1Block} from "./interfaces/IFraxtalL1Block.sol";
 import {IBeefyVault} from "./interfaces/IBeefyVault.sol";
 import {ICurveStableNG} from "./interfaces/ICurveStableNG.sol";
 
 import {CurveMooLib} from "./libraries/CurveMooLib.sol";
 
 /**
-    @author @FarajiOranj
+    @author @4BitLab
     @title Come2Top System Contract.
     @notice Come2Top is a secure, automated, and fully decentralized GameFi platform
         built on top of the Fraxtal Mainnet, Curve Protocol & Beefy Yield Farming Protocol
@@ -102,7 +102,7 @@ contract Come2Top {
     IERC20 public immutable FRAX;
     address public immutable TREASURY;
     address public immutable THIS = address(this);
-    ISuperchainL1Block public immutable FRAXTAL_L1_BLOCK;
+    IFraxtalL1Block public immutable FRAXTAL_L1_BLOCK;
     bytes private constant BYTE_TICKETS =
         hex"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
     // Mainnet
@@ -280,7 +280,7 @@ contract Come2Top {
         currentGameStrat = gameStrat;
         FRAX = IERC20(frax);
         TREASURY = treasury;
-        FRAXTAL_L1_BLOCK = ISuperchainL1Block(fraxtalL1Block);
+        FRAXTAL_L1_BLOCK = IFraxtalL1Block(fraxtalL1Block);
         gameData[ZERO].tickets = BYTE_TICKETS;
         unchecked {
             MAGIC_VALUE = uint160(address(this)) * block.chainid;
@@ -1688,9 +1688,7 @@ contract Come2Top {
     }
 
     function _checkGS(uint8 value) private pure {
-
-        if (value > SIX)
-            revert VALUE_CANT_BE_GREATER_THAN(MIN_TICKET_PRICE);
+        if (value > SIX) revert VALUE_CANT_BE_GREATER_THAN(SIX);
     }
 
     /**
